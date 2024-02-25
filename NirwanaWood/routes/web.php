5f6cf3a412a507a34156dabb10ad8265863f3a46
sofiasfmld <sfmldmaulida9@gmail.com>
2024-02-25 16:08:45 +0700

    dikit

--- NirwanaWood/routes/web.php
+++ NirwanaWood/routes/web.php
@@ -13,43 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::get('/', function () {
-    return view('index');
-});
-
-Route::get('about', function (){
-    return view('about');
-});
-
-Route::get('contact', function (){
-    return view('contact');
-});
-
-Route::get('project', function (){
-    return view('project');
-});
-
-Route::get('feature', function (){
-    return view('feature');
-});
-
-Route::get('quote', function (){
-    return view('quote');
-});
-
-Route::get('service', function (){
-    return view('service');
-});
-
-Route::get('team', function (){
-    return view('team');
-});
-
-Route::get('testimonial', function (){
-    return view('testimonial');
-});
-
-Route::get('/admin', function () {
-    return view('admin.index');
-});
+Route::get('/', function () {return view('index');});
+Route::get('about', function (){return view('about');});
+Route::get('contact', function (){return view('contact');});
+Route::get('project', function (){return view('project');});
+Route::get('feature', function (){return view('feature');});
+Route::get('quote', function (){return view('quote');});
+Route::get('service', function (){return view('service');});
+Route::get('team', function (){return view('team');});
+Route::get('testimonial', function (){return view('testimonial');});
+Route::get('/admin', function () {return view('admin.index');});
+Route::get('/admin.tables', function () {return view('admin.tables');});
